f251848b78afbefcb02ead5f79bef26ff984f202
Ovlic <74332950+Ovlic@users.noreply.github.com>
2021-09-25 19:17:16 -0700

    Delete bot directory

--- bot/commands/repeat.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'repeat',
-    description: "Repeats what the user says",
-    execute(message, args, client) {
-
-        return message.channel.send(args.join(" "))
-
-    },
-  }
